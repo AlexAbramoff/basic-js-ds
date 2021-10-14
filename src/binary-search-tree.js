f6ead9cab3d8f6c2d09c6fd6a7f0322265ab2346
AlexAbramoff <alex93178@gmail.com>
2021-10-14 22:36:47 +0300

    feat: add root method to binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,19 +1,23 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 module.exports = class BinarySearchTree {
-
+  constructor() {
+    this.rootNode = new Node(null)
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode.value) {
+      return null
+    }
+    return this.rootNode
   }
 
-  add(/* data */) {
+  add(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
